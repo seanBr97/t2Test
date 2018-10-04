a0914b05ecb50b8dba7aa5b7d13f20d5c8de4315
seanBr97 <brieffies@tcd.ie>
2018-10-04 22:28:31 +0100

    Fixed Build Errors

--- t2Test/t2.asm
+++ t2Test/t2.asm
@@ -16,13 +16,13 @@ public      min             ; make sure function name is exported
 min:
 		mov rax, rcx		; v = a
 		cmp rdx, rax		; if (b < v)
-		jge min0			: {
+		jge min0			; {
 		mov rax, rdx		; v = b
-min0						; }
+min0:						; }
 		cmp r8, rax			; if (c < v)
-		jge min1			: {
+		jge min1			; {
 		mov rax, r8			; v = c
-min1						; }	
+min1:						; }	
 		ret 0               ; return v in rax	
 
 
@@ -51,16 +51,6 @@ p:
 		ret	0				; return answer in rax
 
 
-_int64 gcd(_int64 a, _int64 b) 
-{
-	if (b == 0) 
-		return a;
-	else 
-		return gcd(b, a % b);
-}
-
-;parameters a - rcx	b - rdx
-
 public		gcd				; export function name
 
 gcd:
@@ -69,7 +59,7 @@ gcd:
 		jne elseB			; {
 		mov rax, rcx		; return a
 		ret 0				; }
-elseB						; else {
+elseB:						; else {
 		mov rax, rcx		; rax = a
 		mov rcx, rdx		; b
 		mov rdx, 0			; clear
@@ -80,18 +70,6 @@ elseB						; else {
 		add rsp, 32			; deallocate shadow space
 		ret 0				; return gcd (b,a%b)
 		
-			rcx		rdx			r8		r9			stack
-_int64 q(_int64 a, _int64 b, _int64 c, _int64 d, _int64 e) 
-{
-	_int64 sum = a + b + c + d + e;
-	printf("a = %I64d b = %I64d c = %I64d d = %I64d e = %I64d sum = %I64d\n", a, b, c, d, e, sum);
-	return sum;
-}
-printf - 
-rcx [address of format string]
-rdx [a]
-r8 [b]
-r9 [a+b]
 
 public		q				; export function name
 
